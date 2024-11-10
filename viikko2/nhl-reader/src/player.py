4f42cce94b2f3c74a824cdb4a1b011ed646c9475
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2024-11-10 17:55:34 +0200

    all attributes added

--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,12 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
-    
+        self.nation = dict['nationality']
+        self.team = dict['team']
+        self.goals = dict['goals']
+        self.assists = dict['assists']
+        self.games = dict['games']
+        self.id = dict['id']
+
     def __str__(self):
         return self.name
